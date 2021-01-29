a0c6fc344292137b391584da63e5c1f62deeaaac
PrateekChaturvedi <prateek_chaturvedi@persistent.com>
2021-01-29 15:53:43 +0530

    Div() implementation

--- math.py
+++ math.py
@@ -1,1 +1,1 @@
-#Additiondef add(x,y):     return x+y#subtractiondef subtract(x,y):     if(x<y):          return "ERROR"     return x-y#multiplicationdef multiply(x,y):     return x*y    #divisiondef divide(x,y):     pass
+#Additiondef add(x,y):     return x+y#subtractiondef subtract(x,y):     if(x<y):          return "ERROR"     return x-y#multiplicationdef multiply(x,y):     return x*y    #divisiondef divide(x,y):     return x/y
